cc256cca303d0f6643fd0629a5d28c30fa84d26c
Gary Ching-Pang Lin <chingpang@gmail.com>
2014-02-19 16:33:00 +0800

    update dump_cert.sh for the newer shim.efi

--- dump_cert.sh
+++ dump_cert.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
 # Usage: dump_cert.sh <efi file> <output>
 INPUT=$1
-OUTPUT=$2
 
-SEC_INFO=`objdump -h -j.data -F $INPUT |grep .data`
+SEC_INFO=`objdump -h -j.vendor_cert -F $INPUT |grep .vendor_cert`
 SEC_ADDR=0x`echo $SEC_INFO | cut -d ' ' -f 4`
 SEC_OFFSET=0x`echo $SEC_INFO | cut -d ' ' -f 6`
-CERT_ADDR=0x`nm $INPUT | grep -w vendor_cert |cut -d ' ' -f 1`
-SIZE_ADDR=0x`nm $INPUT | grep -w vendor_cert_size |cut -d ' ' -f 1`
-CERT_OFFSET=$(($CERT_ADDR - $SEC_ADDR + $SEC_OFFSET))
-SIZE_OFFSET=$(($SIZE_ADDR - $SEC_ADDR + $SEC_OFFSET))
-SIZE=`od -An -j $SIZE_OFFSET -N 4 -t d $INPUT | tr -d ' '`
+CERT_PRIV=0x`nm $INPUT | grep -w vendor_cert_priv |cut -d ' ' -f 1`
+CERT_PRIV_END=0x`nm $INPUT | grep -w vendor_cert_priv_end |cut -d ' ' -f 1`
 
-echo "dd if=$INPUT of=$OUTPUT skip=$CERT_OFFSET count=$SIZE bs=1"
-dd if=$INPUT of=$OUTPUT skip=$CERT_OFFSET count=$SIZE bs=1
+OFFSET=$(($CERT_PRIV - $SEC_ADDR + $SEC_OFFSET))
+SIZE=$(($CERT_PRIV_END - $CERT_PRIV))
+
+CERT_SUM=`dd if=$INPUT skip=$OFFSET count=$SIZE bs=1 2>/dev/null | sha256sum -b | cut -d ' ' -f 1`
+echo $CERT_SUM
