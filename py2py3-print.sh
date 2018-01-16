86f817da6e515c38ae67b7c7f3b272bfdb4fa32c
Gary Lin <glin@suse.com>
2018-01-16 12:28:18 +0800

    Udpate py2py3-print.sh

--- py2py3-print.sh
+++ py2py3-print.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-sed -i -re 's/(\s+print\s+)([[:print:]]+)/\1(\2)/g' $1
-#             ^            ^                ^^^^
-#             |            |                add the parentheses
-#             +--group1    +--group2
+sed -i -re 's/^(\s+print\s+)([[:print:]]+)/\1(\2)/g' $1
+#              ^            ^                ^^^^
+#              |            |                add the parentheses
+#              +--group1    +--group2
 # group1: search for 'print', including the whitespaces before and after 'print'
 # group2: match all the printable characters
 #
