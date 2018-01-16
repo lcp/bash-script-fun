e5b9877002eec2005bf7ebe673f69e57ff4d8988
Gary Lin <glin@suse.com>
2018-01-16 12:18:09 +0800

    Add the sed command to convert python print

---
+++ py2py3-print.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sed -i -re 's/(\s+print\s+)([[:print:]]+)/\1(\2)/g' $1
+#             ^            ^                ^^^^
+#             |            |                add the parentheses
+#             +--group1    +--group2
+# group1: search for 'print', including the whitespaces before and after 'print'
+# group2: match all the printable characters
+#
+# Original: print content
+# Result:   print (content)
